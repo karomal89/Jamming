f4e80d4422716e15f3fdaeb0a814723c7a124a60
karomal89 <karomal89@gmail.com>
2022-03-23 16:33:54 +0000

    created method that saves playlist to users account

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -44,6 +45,11 @@ class App extends React.Component {
     this.setState({ playlistName: name});
   }
 
+  savePlaylist(){
+    
+    const trackUri = this.state.playlistTracksmap(track => track.uri);
+  }
+
 
 
   render() {
@@ -54,7 +60,7 @@ class App extends React.Component {
                 <SearchBar />
              <div className="App-playlist">
                 <SearchResults searchResults={this.state.searchResults}  onAdd={this.addTrack}/>
-                <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/> 
+                <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} /> 
              </div>
              </div>
       </div>
