80631a453e755e358f7f5248c0bc51a058835fae
karomal89 <karomal89@gmail.com>
2022-03-23 19:17:33 +0000

    save users playlist

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -27,11 +27,11 @@ const Spotify = {
     },
     search(term) {
         const accessToken = Spotify.getAccessToken();
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, 
-        { headers: {
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+         headers: {
             Authorization: `Bearer ${accessToken}`
-        }
-        }}).then(response => {
+         }
+        }).then(response => {
             return response.json();
         }).then(jsonResponse => {
             if (!jsonResponse.tracks) {
@@ -43,11 +43,39 @@ const Spotify = {
                 artist: track.artists[0].name,
                 album: track.album.name,
                 uri: track.uri
-            }));
-
-            
-        });
+            }))           
+        })
     },
+    savePlaylist(name, trackUris) {
+        if (!name || !trackUris.length) {
+            return;
+        }
+
+        const accessToken = Spotify.getAccessToken();
+        const headers = { Authorization: `Bearer ${accessToken}`};
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', {headers: headers}
+        ).then(response => response.json()
+        ).then(jsonResponse => {
+            userId = jsonResponse.id;
+             return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,
+            {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({name: name})
+            }).then(response => response.json()
+            ).then(jsonResponse => {
+                const playlistId = jsonResponse.id;
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({ uris: trackUris})
+                })
+                 })
+            })
+    }
 }
+
 
 export default Spotify;
