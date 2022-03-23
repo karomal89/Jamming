ee68fd0c19f6658bed060a2024c8a5fe0e47b47c
karomal89 <karomal89@gmail.com>
2022-03-23 12:11:30 +0000

    updates

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import '../SearchResults/SearchResults.css';
+
+import TrackList from '../TrackList/TrackList';
+
+
+class SearchResults extends React.Component {
+  render () {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                <TrackList tracks={this.props.searchResults}/>
+            </div>
+
+        )
+
+    }
+}
+
+export default SearchResults;
